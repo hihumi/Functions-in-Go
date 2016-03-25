c2c0b12fca327ca7f4ae2bd044e13101bbebb530
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-25 12:58:38 +0900

    make dir and file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./addtwoint"
+	"./multitwoint"
 	"./subtwoint"
 	"fmt"
 )
@@ -9,7 +10,7 @@ import (
 func main() {
 	fmt.Printf("Functions-in-Go\n\n")
 
-	// addtwoint
+	// package: addtwoint
 	fmt.Println("Example: package name: addtwoint, function name: AddTwoInt")
 	fmt.Printf("addtwoint.AddTwoInt(1, 2) is %d\n", addtwoint.AddTwoInt(1, 2))
 
@@ -19,7 +20,7 @@ func main() {
 	fmt.Println(myAddTwoInt)
 	fmt.Println()
 
-	// subtwoint
+	// package: subtwoint
 	fmt.Println("Example: package name: subtwoint, function name: SubTwoInt")
 	fmt.Printf("subtwoint.SubTwoInt(2, 1) is %d\n", subtwoint.SubTwoInt(2, 1))
 
@@ -27,5 +28,15 @@ func main() {
 	mySubTwoInt := subtwoint.SubTwoInt(200, 100)
 	fmt.Printf("Variable name: mySubTwoInt is ")
 	fmt.Println(mySubTwoInt)
+	fmt.Println()
+
+	// package: multitwoint
+	fmt.Println("Example: package name: multitwoint, function name: MultiTwoInt")
+	fmt.Printf("multitwoint.MultiTwoInt(3, 4) is %d\n", multitwoint.MultiTwoInt(3, 4))
+
+	// Assignment
+	myMultiTwoInt := multitwoint.MultiTwoInt(300, -400)
+	fmt.Printf("Variable name: mySubTwoInt is ")
+	fmt.Println(myMultiTwoInt)
 	fmt.Println()
 }
---
+++ multitwoint/multitwoint.go
@@ -0,0 +1,5 @@
+package multitwoint
+
+func MultiTwoInt(x int, y int) int {
+	return x * y
+}
