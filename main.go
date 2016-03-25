fce97762c8c87c548648d00e2901b4e4c2c5b915
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-03-25 09:12:05 +0900

    fix

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Printf("Functions-in-Go\n\n")
 
-    fmt.Println("Example: package name: addtwoint, function name: AddTwoInt")
-    fmt.Printf("addtwoint.AddTwoInt(1, 2) is %d\n", addtwoint.AddTwoInt(1, 2))
-    fmt.Println()
+	fmt.Println("Example: package name: addtwoint, function name: AddTwoInt")
+	fmt.Printf("addtwoint.AddTwoInt(1, 2) is %d\n", addtwoint.AddTwoInt(1, 2))
+	fmt.Println()
 }
